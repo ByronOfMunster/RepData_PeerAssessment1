5c8b59cd51bacbaf2ddddc5598d619ef3a5b63b3
Byron Estes <byronestes@gmail.com>
2015-05-07 13:44:17 -0700

    Save to avoid loss

--- script.R
+++ script.R
@@ -26,29 +26,57 @@ activity <- read.csv(unz(zipfile, "activity.csv"),
                      header=TRUE, 
                      sep =",",
                      na.strings="NA",
-                     stringsAsFactors=FALSE)
+                     stringsAsFactors=TRUE)
 
 
-# What is mean total number of steps taken per day?
+activity$date <- ymd(activity$date)
 
-activity_avg_by_date <- activity %>%
+# What is mean total number of steps taken per day? (Sum)
+# Data represents observations for one individual.
+activity_sum_by_date <- activity %>%
         group_by(date) %>%
-        summarize(mean(steps, na.rm=TRUE))
+        summarize(sum(steps, na.rm=TRUE))
 
-names(activity_avg_by_date) <- c("Date", "Mean")
+names(activity_sum_by_date) <- c("Date", "Steps")
 
 png(filename="before_date.png",
     width=480,
     height=480)
 
-hist(activity_avg_by_date$Mean)
+bins <- hist(activity_sum_by_date$Steps, breaks=20, col="lightblue3", xlab="Sum of Steps per Day", main="Distibution of Sum of Steps Per Day" )
+text(x=bins$mids, 
+     y=bins$count,
+     label=bins$count,
+     col="darkblue",
+     font=1)
 
 dev.off()
 
+
+# Put together sum, mean and median for printing into a table in markup document.
+names(activity_sum_by_date) <- c("Date", "Sum")
+
+activity_mean_by_date <- activity %>%
+        group_by(date) %>%
+        summarize(mean(steps, na.rm=TRUE))
+
+names(activity_mean_by_date) <- c("Date", "Mean")
+
+activity_median_by_date <- activity %>%
+        group_by(date) %>%
+        summarize(median(steps, na.rm=TRUE))
+
+names(activity_median_by_date) <- c("Date", "Median")
+
+step_stats_by_date <- cbind(activity_sum_by_date, activity_mean_by_date$Mean, activity_median_by_date$Median)
+names(step_stats_by_date) <- c("Date", "Sum", "Mean", "Median")
+print(step_stats_by_date)
+
 #barplot(activity_avg_by_date$Mean)
 
-# What is the average daily activity pattern? (Average per 5 minute slice across all days?)
 
+
+# What is the average daily activity pattern? (Average per 5 minute slice across all days?)
 activity_avg_by_interval <- activity %>%
         group_by(interval) %>% 
         summarize(mean(steps, na.rm=TRUE))
@@ -60,7 +88,21 @@ png(filename="before_interval.png",
     width=480,
     height=480)
 
+####NOT to be used!!!!!!!!!!!!!!!!######
 hist(activity_avg_by_interval$Mean)
+
+bins <- hist(activity_avg_by_interval$Mean, breaks=288, col="lightblue3", xlab="Sum of Steps per Day", main="Distibution of Sum of Steps Per Day" )
+text(x=bins$mids, 
+     y=bins$count,
+     label=bins$count,
+     col="darkblue",
+     font=1)
+
+plot(activity_avg_by_interval$Interval, activity_avg_by_interval$Mean, type="l")
+
+
+activity_avg_by_interval[which.max(activity_avg_by_interval$Mean),]
+
 
 dev.off()
 
@@ -76,10 +118,10 @@ nrow(activity_na_steps)
 # no average for a NA step, then set to zero.
 
 
-Loop through activity_avg_by_interval
-subselect observations with that interval and NA ---replace it with the mean
-end loop
-select any remaining na and set to zero (...or eliminate) 
+#Loop through activity_avg_by_interval
+#subselect observations with that interval and NA ---replace it with the mean
+#
+#select any remaining na and set to zero (...or eliminate) 
 
 activity_imputed <- activity
 
@@ -92,7 +134,7 @@ for(i in 1:nrow(activity_avg_by_interval)) {
         #print (paste(i, interval,average))
 }
 
-
+#Use same diagrams above REPLACE!!!!!!!!!!!!!as neede!!!!!!!!!!!!!!!!!!
 
 activity_imputed_avg_by_date <- activity_imputed %>%
         group_by(date) %>%
@@ -130,7 +172,8 @@ http://stackoverflow.com/questions/9322773/how-to-replace-na-with-mean-by-subset
 #Add day of week to dataset
 activity_imputed_with_dayofweek <- activity_imputed %>%
         mutate(day=wday(date)) %>%
-        group_by(day) %>%
+        mutate(weekday=ifelse(day == (1 | 7), "weekend", "weekday")) %>%
+        group_by(weekday) %>%
         summarize(mean(steps))        
 
 png(filename="after_day_of_week.png",
@@ -149,14 +192,10 @@ barplot(activity_imputed_with_dayofweek$Mean,
 
 dev.off()
 
-!!!!!
-        TODO:
-        !!!!
-        Create a new factor variable in the dataset with two levels – “weekday” and “weekend” indicating
-whether a given date is a weekday or weekend day.
-2. Make a panel plot containing a time series plot (i.e. type = "l" ) of the 5minute
-interval (xaxis)
-and
-the average number of steps taken, averaged across all weekday days or weekend days (yaxis).
-See the README file in the GitHub repository to see an example of what this plot should look like
-using simulated data.
+#!!!!!TODO:!!!!
+#Create a new factor variable in the dataset with two levels – “weekday” and “weekend” indicating
+#whether a given date is a weekday or weekend day.
+#2. Make a panel plot containing a time series plot (i.e. type = "l" ) of the 5minute
+#interval (xaxis) and the average number of steps taken, averaged across all weekday days or weekend days (yaxis).
+#See the README file in the GitHub repository to see an example of what this plot should look like
+#using simulated data.
